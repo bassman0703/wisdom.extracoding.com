c79818781906aa08160df03ab02208d2ed151aec
daviti <davitijmukhadze@gmail.com>
2020-08-19 20:25:09 +0400

    add:some.

--- js/paypal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.options .button').click(function () {
-        $('.options .button').removeClass('button--active');
-        $(this).addClass('button--active');
-        var value = $(this).text().replace('$', '');
-        $('#donate-field input').val(value);
-    });
-});
